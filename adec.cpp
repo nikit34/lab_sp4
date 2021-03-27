99144c68f9aaf4ebd21b8b5951f5b827fc0fb513
Никита Пермяков <permikov134@mail.ru>
2021-03-27 11:13:48 +0300

    proga

--- adec.cpp
+++ adec.cpp
@@ -9,26 +9,28 @@ int main()
 {
   tFSM Adec;
 ///////////////////////
-//удалить этот код и заменить своим
-  addrange(Adec,0,'0','9',1);
-  addrange(Adec,1,'0','9',1);
-  addstr(Adec,1,".",2);
-  addrange(Adec,2,'0','9',2);
- Adec.final(2);
+    addstr(Adec, 0, "+-", 1);
+    addrange(Adec, 1, '0', '9', 1);
+    addrange(Adec, 1, '0', '9', 2);
+    addstr(Adec, 2, ".", 3);
+    addrange(Adec, 3, '0', '9', 3);
+    addrange(Adec, 3, '0', '9', 4);
+    addstr(Adec, 4, "eE", 5);
+    addstr(Adec, 4, "eE", 6);
+    addstr(Adec, 5, "+-", 6);
+    addrange(Adec, 6, '0', '9', 7);
+    Adec.final(7);
 ///////////////////////
   cout << "Number of states = " << Adec.size()
        << "\n";
 
-  while(true)
- {
-  char input[81];
-  cout << ">";
-  cin.getline(input,81);
-  if(!*input) break;
-  int res = Adec.apply(input);
-  cout << setw(res?res+1:0) << "^"
-       << endl;
- }
+  while(true) {
+    char input[81];
+    cout << ">";
+    cin.getline(input,81);
+    if(!*input) break;
+    int res = Adec.apply(input);
+    cout << setw(res ? res + 1 : 0) << "^" << endl;
+  }
  return 0;
 }
-
