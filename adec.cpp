0999f9133c6f47208e62428648842524a3812f90
nikit34 <permikov134@mail.ru>
2021-03-30 00:05:41 +0300

    reight

--- adec.cpp
+++ adec.cpp
@@ -1,25 +1,28 @@
 
-//               adec.cpp
+// adec.cpp
 #include <iostream>
 #include <iomanip>
 #include "fsm.h"
+
 using namespace std;
+
 
 int main()
 {
   tFSM Adec;
-///////////////////////
-    addstr(Adec, 0, "+-", 1);
+// 24|  *  |     +      | + |        +      | + |  *  |  +  | 0.00001e2
+    addstr(Adec, 0, "+-", 2);
+    addrange(Adec, 0, '0', '9', 1);
     addrange(Adec, 1, '0', '9', 1);
-    addrange(Adec, 1, '0', '9', 2);
-    addstr(Adec, 2, ".", 3);
-    addrange(Adec, 3, '0', '9', 3);
+    addrange(Adec, 2, '0', '9', 1);
+    addstr(Adec, 1, ".", 3);
     addrange(Adec, 3, '0', '9', 4);
+    addrange(Adec, 4, '0', '9', 4);
     addstr(Adec, 4, "eE", 5);
-    addstr(Adec, 4, "eE", 6);
-    addstr(Adec, 5, "+-", 6);
-    addrange(Adec, 6, '0', '9', 7);
-    Adec.final(7);
+    addstr(Adec, 5, "+-", 7);
+    addrange(Adec, 5, '0', '9', 6);
+    addrange(Adec, 7, '0', '9', 6);
+    Adec.final(6);
 ///////////////////////
   cout << "Number of states = " << Adec.size()
        << "\n";
